53f5fecdddea0793fefc7ec4f1f462d2bcfe854b
wkisielewicz <wkisielewicz@criticue.com>
2016-10-22 21:18:05 +0200

    Main controller for posts.

--- src/nahim/controllers/posts.clj
+++ src/nahim/controllers/posts.clj
@@ -1,3 +1,14 @@
-(ns nahim.controllers.posts)
+(ns nahim.controllers.posts
+  (:require
+    [clostache.parser :as clostache]
+    [nahim.models.posts :as posts-model]))
 
-(defn index [] "Hello, from index")
+(defn read-template [template-name]
+  (slurp (clojure.java.io/resource
+    (str "views/posts/" template-name ".mustache"))))
+
+(defn render-template [template-file params]
+  (clostache/render (read-template template-file) params))
+
+  (defn index []
+    (render-template "index" {:posts (posts-model/all)}))
