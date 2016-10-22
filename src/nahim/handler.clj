84b58bbf0149725c82374c3abd3de01b33d9f614
wkisielewicz <wkisielewicz@criticue.com>
2016-10-22 21:17:04 +0200

    Add handle.clj with authentication.

--- src/nahim/handler.clj
+++ src/nahim/handler.clj
@@ -1,13 +1,41 @@
 (ns nahim.handler
-  (:require [compojure.core :refer :all]
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
             [compojure.route :as route]
+            [ring.middleware.basic-authentication :refer :all]
+            [ring.util.response :as resp]
+            [nahim.models.posts :as posts-model]
             [nahim.controllers.posts :as posts-controller]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [nahim.controllers.admin.posts :as admin-posts-controller]))
+
+(defn authenticated? [name pass]
+  (and (= name "user")
+       (= pass "pass")))
+
+(defroutes public-routes
+  (GET "/" [] (posts-controller/index))
+  (route/resources "/" ))
+
+(defroutes protected-routes
+  (GET "/admin" [] (admin-posts-controller/index))
+  (GET "/admin" [id] (admin-posts-controller/show id))
+  (GET "/admin/posts/new" [] (admin-posts-controller/new))
+  (POST "/admin/posts/create" [& params]
+    (do (posts-model/create params)
+        (resp/redirect "/admin")))
+  (GET "/admin/posts/:id/edit" [id] (admin-posts-controller/edit id))
+  (POST "/admin/posts/:id/save" [& params]
+    (do (posts-model/save (:id params) params)
+        (resp/redirect "/admin")))
+  (GET "/admin/posts/:id/delete" [id]
+    (do (posts-model/delete id)
+      (resp/redirect "/admin"))))
 
 (defroutes app-routes
-  (GET "/" [] (posts-controller/index))
-  (route/resources "/")
-  (route/not-found "Not Found"))
+  public-routes
+  (wrap-basic-authentication protected-routes authenticated?)
+  (route/not-found "404 Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (handler/site app-routes))
+Now lets add basic auth depende
