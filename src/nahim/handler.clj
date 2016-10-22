7c8b0cffe6eaf60df6322a5cf96db23804576a68
wkisielewicz <wkisielewicz@criticue.com>
2016-10-22 17:03:49 +0200

    Add route resources in handler.clj.

--- src/nahim/handler.clj
+++ src/nahim/handler.clj
@@ -1,10 +1,12 @@
 (ns nahim.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [nahim.controllers.posts :as posts-controller]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (posts-controller/index))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
