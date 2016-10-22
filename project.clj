dde64fc5462c2a6796638e53881612f393844f9d
wkisielewicz <wkisielewicz@criticue.com>
2016-10-22 21:01:13 +0200

    Update project.clj ring-basic authentication.

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [de.ubercode.clostache/clostache "1.3.1"]
+                 [org.clojure/java.jdbc "0.3.0-alpha5"]
+                 [mysql/mysql-connector-java "5.1.25"]
+                 [ring-basic-authentication "1.0.2"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler nahim.handler/app}
   :profiles
